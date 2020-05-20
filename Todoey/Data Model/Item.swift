23caa5a8f5577d32db6f5788b0afae6350291809
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-05-20 23:18:10 +0900

    Implement Item class

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 1101373 on 2020/05/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
