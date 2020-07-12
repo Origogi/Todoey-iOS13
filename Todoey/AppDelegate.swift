81eab401040fd79352c74bfd6bfb3f5b23e68912
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-07-12 12:36:07 +0900

    Add Realm library

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -6,32 +6,48 @@
 //  Copyright (c) 2015 London App Brewery. All rights reserved.
 //
 
-import UIKit
 import CoreData
+import RealmSwift
+import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "Origogi"
+        data.age = 33
+        
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
+
+        } catch {
+            print("Error initialising new realm, \(error)")
+            
+        }
+        
+
+
         return true
     }
 
-
     func applicationWillTerminate(_ application: UIApplication) {
-        self.saveContext()
+        saveContext()
     }
 
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
-
         let container = NSPersistentContainer(name: "DataModel")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: { _, error in
             if let error = error as NSError? {
-
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
@@ -40,7 +56,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Core Data Saving support
 
-    func saveContext () {
+    func saveContext() {
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
@@ -51,6 +67,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
-
---
+++ Todoey/Data Model/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by 1101373 on 2020/07/12.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    dynamic var name: String = ""
+    dynamic var age: Int = 0
+}
